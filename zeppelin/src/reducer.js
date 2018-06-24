90be683cba7b9db0e53bd7353b23f0cb9a648a0b
Julliusz Jakubowski <juliusz.jakubowski@gmail.com>
2018-06-24 12:31:06 +0200

    adding payload to store

--- zeppelin/src/reducer.js
+++ zeppelin/src/reducer.js
@@ -1,5 +1,18 @@
 import {combineReducers} from 'redux';
 
+const projects = (state = {loading: false, data: []}, action) => {
+    switch (action.type) {
+        case 'FETCH_PROJECTS_REQUEST':
+            return {...state, loading: true};
+        case "FETCH_PROJECTS_FAILURE":
+            return { ...state, loading: false };
+        case 'FETCH_PROJECTS_SUCCESS':
+            return {...state, loading: false, data: action.payload};
+        default:
+            return state;
+    }
+};
+
 const isAppLoading = (state = false, action) => {
     switch (action.type) {
         case 'APP_INIT' :
@@ -9,4 +22,4 @@ const isAppLoading = (state = false, action) => {
     }
 };
 
-export default combineReducers({isAppLoading});
+export default combineReducers({isAppLoading, projects});
